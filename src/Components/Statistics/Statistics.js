52417867b850f79004c65411533ca870362102b3
tanuit <dastanushri402@gmail.com>
2022-10-12 01:01:22 +0600

    nine

--- src/Components/Statistics/Statistics.js
+++ src/Components/Statistics/Statistics.js
@@ -1,15 +1,33 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
-import Chart from "../Chart/Chart";
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from "recharts";
+import "./Statistics.css";
 
 const Statistics = () => {
-  const totalQuiz = useLoaderData();
-  console.log(totalQuiz.data);
+  const data = useLoaderData();
+
   return (
-    <div>
-      {totalQuiz.data.map((quiz) => (
-        <Chart key={quiz.id} quiz={quiz}></Chart>
-      ))}
+    <div className="chart">
+      <LineChart
+        className="width"
+        width={600}
+        height={300}
+        data={data.data}
+        margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+      >
+        <Line type="monotone" dataKey="total" stroke="#8884d8" />
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+      </LineChart>
     </div>
   );
 };
