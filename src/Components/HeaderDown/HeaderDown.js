5cd19ddc7413c379ee1079afeeb15d8bc5f720bc
tanuit <dastanushri402@gmail.com>
2022-10-12 20:15:32 +0600

    poli

--- src/Components/HeaderDown/HeaderDown.js
+++ src/Components/HeaderDown/HeaderDown.js
@@ -4,14 +4,16 @@ import "./HeaderDown.css";
 const HeaderDown = () => {
   return (
     <div className="header-dawn">
-      <p className="text">
-        Education is a purposeful activity directed at achieving certain aims,
-        such as transmitting knowledge or fostering skills and character traits.
-        These aims may include the development of understanding, rationality,
-        kindness, and honesty.Our mission is to promote student achievement and
-        preparation for global competitiveness by fostering educational
-        excellence and ensuring equal access.
-      </p>
+      <div className="text">
+        <p>
+          Education is a purposeful activity directed at achieving certain aims,
+          such as transmitting knowledge or fostering skills and character
+          traits. These aims may include the development of understanding,
+          rationality, kindness, and honesty.Our mission is to promote student
+          achievement and preparation for global competitiveness by fostering
+          educational excellence and ensuring equal access.
+        </p>
+      </div>
     </div>
   );
 };
